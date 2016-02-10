b843f46afc25bd2c6955bf35de62ed6751a44a7e
BuildTools <asydix@gmail.com>
2016-02-09 23:51:37 -0330

    Essentials Script is Complete.

--- Essentials/2.0.1-b267/essentials-2.0.1-b267.py
+++ Essentials/2.0.1-b267/essentials-2.0.1-b267.py
@@ -4,12 +4,12 @@ import os
 _plugin_versions_ = ['2.0.1-b267']
 _plugin_id_ = "9089"  # Spigot resource ID
 
-__config_template__ = "https://raw.githubusercontent.com/TechnicalBro/minecraft-plugin-config-templates/master/Commons/1.8.8-3/config.%s"
-__config_defaults__ = "https://raw.githubusercontent.com/TechnicalBro/minecraft-plugin-config-templates/master/Commons/1.8.8-3/defaults.yml"
+__config_template__ = "https://raw.githubusercontent.com/TechnicalBro/minecraft-plugin-config-templates/master/Essentials/2.0.1-b267/essentials-template.yml"
+__config_defaults__ = "https://raw.githubusercontent.com/TechnicalBro/minecraft-plugin-config-templates/master/Essentials/2.0.1-b267/essentials-defaults.yml"
 
 
 def configure(parent_folder, config_options={}, **kwargs):
-    commons_folder = os.path.join(parent_folder, 'Commons')
+    commons_folder = os.path.join(parent_folder, 'Essentials')
 
     if not os.path.exists(commons_folder):
         os.makedirs(commons_folder)
@@ -20,15 +20,9 @@ def configure(parent_folder, config_options={}, **kwargs):
     # Create a full dictionary of all the options required to render the template, merging
     # in the missing values from the default config.
     options = merge_configuration_options(config_options, defaults)
+    config_file = os.path.join(commons_folder, 'config.yml')
 
-    # Commons supports multiple configuration options, yml and xml (likely more in the future)
-    # So a kwarg is optionally passed to specify the type of configuration to render.
-    config_type = kwargs.get('config_type', 'yml')
-
-    config_file = os.path.join(commons_folder, 'config.%s' % config_type)
-
-    # Render the configuration of Commons from the url, with the options (and defaults included)
-    commons_config = render_config_from_url(__config_template__ % config_type, options)
+    essentials_config = render_config_from_url(__config_template__, options)
     # Lastly write the configuration to the file specified!
-    write_file(config_file, commons_config)
-    print("Configuration for Commons 1.8.8-3 has been rendered!")
+    write_file(config_file, essentials_config)
+    print("Configuration for Essentials has been rendered!")
